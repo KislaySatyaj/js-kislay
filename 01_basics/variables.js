8eee8ac42e8525d1f7f3e4d7fc5d8c2fa0db6ace
KislaySatyaj <127678096+KislaySatyaj@users.noreply.github.com>
2023-10-25 20:39:42 +0000

    variables done

---
+++ 01_basics/variables.js
@@ -0,0 +1,18 @@
+const accountId= 12112
+let accountEmail="kislaysatyaj@gmail.com"
+var accountPassword="12346"
+accountCity="ranchi"
+let accountState // will say that this variable is undefinied
+
+//accountId=2 //not allowed
+
+accountEmail="jc@gmailk.com"
+accountPassword="364876478"
+accountCity="Bengaluru"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/*
+prefer not to use var because of use in block scope and functional scope
+*/
+
