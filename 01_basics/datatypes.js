8d9e4278b7ac52c07ba0a7a21debd292883e4e00
KislaySatyaj <127678096+KislaySatyaj@users.noreply.github.com>
2023-10-30 07:07:35 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) // we are using nodejs , not browser
+
+console.log(3 +
+    3)// code readibility should be high,not a good practice
+
+let name="kislay"
+let age=21
+let isLoggedIn= false
+
+// number => 2 to power 53
+//bigint
+//string =>"" or''
+//boolean=> true/false
+// null => standalone value, it means that it is not undefined but has no value
+// undefined => when value is not assigned
+// symbol=> unique(components uniqueness)
+// the above mentioned are primitive datatypes
+
+
+//object
+
+console.log(typeof "kislay")
+console.log(typeof age)
+
+console.log(typeof null) // type is object
+console.log(typeof undefined) // type is unidentified
